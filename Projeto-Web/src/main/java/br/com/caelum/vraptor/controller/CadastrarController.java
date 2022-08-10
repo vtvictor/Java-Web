5142181484d47511efdf7ed843b12cce1450a81d
vtvictor <victorluiz.bsi@gmail.com>
2022-08-10 16:21:09 -0300

    First Commit

---
+++ Projeto-Web/src/main/java/br/com/caelum/vraptor/controller/CadastrarController.java
@@ -0,0 +1,16 @@
+package br.com.caelum.vraptor.controller;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Path;
+
+@Path("cadastrar")
+@Controller
+public class CadastrarController {
+	
+	@Get("")
+	public void cadastrar() {
+		
+	}
+
+}
